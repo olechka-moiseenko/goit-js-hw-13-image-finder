b58885c726c7600690bfac7e3bf9c9c3a43cf099
olechka-moiseenko <73791863+olechka-moiseenko@users.noreply.github.com>
2021-05-30 12:45:56 +0300

    Changes in html

--- src/api-servise.js
+++ src/api-servise.js
@@ -1,18 +1,42 @@
+const API_KEY = '21833182-c50b5ade5b44b638c37fcd74e';
+const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
+const options = {
+  headers: {
+    Authorization: API_KEY,
+  },
+};
+
 export default class ApiService {
-    constructor() {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
 
-    }
+  fetchImages() {
+    const url = `${BASE_URL}/?image_type=photo&orientation=horizontal
+      &q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`;
 
-    fetchImages() {
-        const options = {
-            headers: {
-                Autorization: '21833182-c50b5ade5b44b638c37fcd74e',
-            },
-        };
-        const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal`;
+    return fetch(url, options)
+      .then(response => response.json())
+      .then(({ images }) => {
+        this.incrementPage();
+        return images;
+      });
+  }
 
-        fetch(url, options)
-        .then(r=>r.json)
-        .then(console.log);
-    };
+  incrementPage() {
+    this.page++;
+  }
 
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuerry) {
+    this.searchQuery = newQuerry;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './sass/main.scss';
+import './api-servise.js';
 
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  imageGallerey: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+};
+const service = new ApiService();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefaut();
+  searchQuery = e.currentTarget.elements.query.value;
+  service.fetchImages(searchQuery);
+}
+
+function onLoadMore() {
+  service.fetchImages();
+}
